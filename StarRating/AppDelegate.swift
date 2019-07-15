cf4b602888451499b506358c696544278b7fb4e7
Frank van Boheemen <frankvb@gmail.com>
2019-07-15 10:52:18 +0200

    Initial Commit

---
+++ StarRating/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StarRating
+//
+//  Created by Frank van Boheemen on 15/07/2019.
+//  Copyright © 2019 Frank van Boheemen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StarRating/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by Frank van Boheemen on 15/07/2019.
+//  Copyright © 2019 Frank van Boheemen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
